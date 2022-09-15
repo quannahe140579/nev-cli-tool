bc4ac5fa8de14eeeee4f67a53a15e3af40d04613
quannahe140579 <70199413+quannahe140579@users.noreply.github.com>
2022-09-16 02:02:12 +0700

    a

--- AzCliGenTool/models.py
+++ AzCliGenTool/models.py
@@ -2,7 +2,6 @@ from abc import ABC
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from . import AzureCliFactory
 from . import const
 import abc
 
